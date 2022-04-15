4e908ce7bc68bd04a9ea804f5beee7e4f37b8c0b
KB <kb19900709@gmail.com>
2022-04-16 03:44:23 +0800

    add text color

--- price.sh
+++ price.sh
@@ -12,17 +12,23 @@ round() {
 }
 
 pairBase="USDT"
-precision=2
+precision=3
+
+red='\033[1;31m'
+green='\033[1;32m'
+cyan='\033[1;36m'
+gray='\033[0;37m'
+nc='\033[0m'
 
 echo 
 
 for crypto in "${myCryptoArray[@]}"; do
     pair="${crypto}${pairBase}"
-    cryptoData=$(curl "https://api.binance.com/api/v3/ticker/24hr?symbol=$pair" -s | jq '. | {"lastPrice":.lastPrice, "highPrice":.highPrice, "lowPrice":.lowPrice, "priceChangePercent":.priceChangePercent}')
-    lastPrice=$(round $(echo $cryptoData | jq '.lastPrice'| tr -d '"') ${precision})
-    highPrice=$(round $(echo $cryptoData | jq '.highPrice'| tr -d '"') ${precision})
-    lowPrice=$(round $(echo $cryptoData | jq '.lowPrice'| tr -d '"') ${precision})
-    priceChangePercent=$(round $(echo $cryptoData | jq '.priceChangePercent'| tr -d '"') ${precision})
-    echo "${crypto}/${pairBase} "\\t" $lastPrice / $priceChangePercent% "\\t" high=$highPrice / low=$lowPrice"
+    cryptoData=$(curl "https://api.binance.com/api/v3/ticker/24hr?symbol=$pair" -s | jq '. | {"lastPrice":.lastPrice, "highPrice":.highPrice, "lowPrice":.lowPrice, "priceChangePercent":.priceChangePercent, "quoteVolume":.quoteVolume}')
+    lastPrice=$(round $(echo $cryptoData | jq '.lastPrice'| tr -d '"') $precision)
+    highPrice=$(round $(echo $cryptoData | jq '.highPrice'| tr -d '"') $precision)
+    lowPrice=$(round $(echo $cryptoData | jq '.lowPrice'| tr -d '"') $precision)
+    priceChangePercent=$(round $(echo $cryptoData | jq '.priceChangePercent'| tr -d '"') $precision)
+    echo "${cyan}${crypto}/${pairBase}${nc} "\\t" ${gray}${lastPrice} / ${priceChangePercent}% "\\t" high=$highPrice / low=$lowPrice ${nc}"
 done
 
