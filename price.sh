255c13e6836c0e763b7cbaa5bbbd96e4e5db28cf
KB <kb19900709@gmail.com>
2022-04-15 17:20:21 +0800

    add highPrice, lowPrice and priceChangePercent

--- price.sh
+++ price.sh
@@ -12,13 +12,17 @@ round() {
 }
 
 pairBase="USDT"
-precision=3
+precision=2
 
 echo 
 
 for crypto in "${myCryptoArray[@]}"; do
     pair="${crypto}${pairBase}"
-    value=$(round $(curl "https://api.binance.com/api/v3/ticker/price?symbol=$pair" -s | jq '.price' | tr -d '"') ${precision})
-    echo $crypto/$pairBase '\t' $value
+    cryptoData=$(curl "https://api.binance.com/api/v3/ticker/24hr?symbol=$pair" -s | jq '. | {"lastPrice":.lastPrice, "highPrice":.highPrice, "lowPrice":.lowPrice, "priceChangePercent":.priceChangePercent}')
+    lastPrice=$(round $(echo $cryptoData | jq '.lastPrice'| tr -d '"') ${precision})
+    highPrice=$(round $(echo $cryptoData | jq '.highPrice'| tr -d '"') ${precision})
+    lowPrice=$(round $(echo $cryptoData | jq '.lowPrice'| tr -d '"') ${precision})
+    priceChangePercent=$(round $(echo $cryptoData | jq '.priceChangePercent'| tr -d '"') ${precision})
+    echo "${crypto}/${pairBase} "\\t" $lastPrice / $priceChangePercent% "\\t" high=$highPrice / low=$lowPrice"
 done
 
